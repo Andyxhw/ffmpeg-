f809092708a72b47d4bd05f1f8eeadda3560635d
xhw <179141106@qq.com>
2020-07-21 18:59:20 -0700

    ffmpeg loop push

---
+++ cmake-build-debug/CMakeFiles/1ffmpegDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/1ffmpegDemo"
+  "../bin/1ffmpegDemo.pdb"
+  "CMakeFiles/1ffmpegDemo.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1ffmpegDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ffmpegDewmo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/ffmpegDewmo"
+  "../bin/ffmpegDewmo.pdb"
+  "CMakeFiles/ffmpegDewmo.dir/src/main_.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ffmpegDewmo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
